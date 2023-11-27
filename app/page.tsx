e52e950c6e64fda72efeae6c90c6d5e819cc01ca
Abraham Galue <abrahamgalue512@gmail.com>
2023-11-27 09:56:26 -0400

    style: change Image component to img element

--- app/page.tsx
+++ app/page.tsx
@@ -32,14 +32,14 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image
+          <img
             src="/hero-desktop.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-          <Image
+          <img
             src="/hero-mobile.png"
             width={560}
             height={620}
