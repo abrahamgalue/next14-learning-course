7f918caa9db2daf3dc67136fc583dd17d4773e7b
Abraham Galue <abrahamgalue512@gmail.com>
2024-07-24 10:54:10 -0400

    Added page to display customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,30 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<TableRowSkeleton />}>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
